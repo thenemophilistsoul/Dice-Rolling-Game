b8602aab856f9e64f536ad569e5e9e21895c5b9c
Amal Santhosh <amalsanthosh84@gmail.com>
2023-01-25 14:45:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor( Math.random() * 6 ) +1;
+var randomNumber2 = Math.floor( Math.random() * 6 ) +1;
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+
+var randomImageSrc1 = "images/"+randomDiceImage1;
+var randomImageSrc2 = "images/"+randomDiceImage2;
+
+
+ var image1 = document.querySelectorAll("img")[0];
+ image1.setAttribute("src",randomImageSrc1);
+ var image2 = document.querySelectorAll("img")[1];
+ image2.setAttribute("src",randomImageSrc2);
+
+
+if(randomNumber1>randomNumber2){
+ document.querySelector("h1").textContent= "Player 1 wins!";
+}
+else if (randomNumber2>randomNumber1){
+    document.querySelector("h1").textContent= "Player 2 wins!";
+}
+else{
+    document.querySelector("h1").textContent= "Its a Tie!";
+}
+
